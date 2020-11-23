63a0b7f41b9fddc7b1c017b125fb8ceb6d32874b
julianurregoar <julian.urrego.ar@gmail.com>
2020-11-22 21:48:56 -0500

    deploy 4

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,21 +1,17 @@
-import { Footer } from "-/src/components/Footer/Footer"
 import { OurBrand } from "-/src/components/OurBrand/OurBrand"
 import { Products } from "-/src/components/Products/Products"
-import Header from "next/head"
 import { PageHeader } from "../../components/Header/PageHeader"
 import { Layout } from "../../Layout/Layout"
-
-import styles from './Home.module.scss'
 
 export const Home = () => {
   return (
     <>
-      <Layout>
-        <PageHeader />
-        <Products title={"Hogar"} />
-        <OurBrand />
-        <Products title={'Industria'} />
-      </Layout>
+      {/* <Layout> */}
+      <PageHeader />
+      <Products title={"Hogar"} />
+      <OurBrand />
+      <Products title={'Industria'} />
+      {/* </Layout> */}
     </ >
   )
 }
