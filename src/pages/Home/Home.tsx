5099511d65b5905d73a32c39999c491151afd382
julianurregoar <julian.urrego.ar@gmail.com>
2020-12-06 17:17:34 -0500

    Added BasciLayout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,18 @@
+import { BasicLayout } from "-/src/BasicLayout/BasicLayout"
 import { OurBrand } from "-/src/components/OurBrand/OurBrand"
 import { Products } from "-/src/components/Products/Products"
-import { Layout } from "-/src/Layout/Layout"
 import { PageHeader } from "../../components/Header/PageHeader"
 
 
 export const Home = () => {
   return (
     <>
-      <Layout>
+      <BasicLayout>
         <PageHeader />
         <Products title={"Hogar"} />
         <OurBrand />
         <Products title={'Industria'} />
-      </Layout>
+      </BasicLayout>
     </ >
   )
 }
