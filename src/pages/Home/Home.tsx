2b86d2ce25d4fbc61109aca29e86c3158125e25f
julianurregoar <julian.urrego.ar@gmail.com>
2020-12-06 16:35:08 -0500

    Removed layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,17 @@
 import { OurBrand } from "-/src/components/OurBrand/OurBrand"
 import { Products } from "-/src/components/Products/Products"
-import { Layout } from "-/src/Layout/Layout"
 import { PageHeader } from "../../components/Header/PageHeader"
 
 
 export const Home = () => {
   return (
     <>
-      <Layout>
+      {/* <Layout> */}
         <PageHeader />
         <Products title={"Hogar"} />
         <OurBrand />
         <Products title={'Industria'} />
-      </Layout>
+      {/* </Layout> */}
     </ >
   )
 }
